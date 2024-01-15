6e328b72d50b5b7c796ef6dcbd4648889a034ad3
Ohleesang <jammmmed@gmail.com>
2024-01-15 17:05:48 +0900

    Feat : 임시로 Dialog 부르는 버튼 추가

--- app/src/main/java/com/example/colorcontacts/MainActivity.kt
+++ app/src/main/java/com/example/colorcontacts/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.colorcontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.colorcontacts.databinding.ActivityMainBinding
+import com.example.colorcontacts.dialog.AddContactDialog
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            AddContactDialog(this).show()
+        }
     }
 }
