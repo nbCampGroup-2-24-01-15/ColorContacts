eaaef58c1bbf0291505c6a0c205b14cf67c073a8
Ohleesang <jammmmed@gmail.com>
2024-01-15 19:28:40 +0900

    Feat : AddContactDialog에 유효성 검사 추가
    텍스트 입력 후 에 유효성 검사
    만약 검사가 되지않을경우 버튼을 비활성화

--- app/src/main/java/com/example/colorcontacts/dialog/AddContactDialog.kt
+++ app/src/main/java/com/example/colorcontacts/dialog/AddContactDialog.kt
@@ -3,11 +3,20 @@ package com.example.colorcontacts.dialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Window
+import com.example.colorcontacts.CheckString
 import com.example.colorcontacts.databinding.DialogAddContactBinding
 
-class AddContactDialog(context : Context) : Dialog(context) {
-    private val binding by lazy{DialogAddContactBinding.inflate(layoutInflater)}
+class AddContactDialog(context: Context) : Dialog(context) {
+    private val binding by lazy { DialogAddContactBinding.inflate(layoutInflater) }
+
+    //유효성 검사 체크 변수들
+    private var isChecked = false
+    private var isCheckedName = false
+    private var isCheckedPhoneNum = false
+    private var isCheckedEmail = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,16 +29,25 @@ class AddContactDialog(context : Context) : Dialog(context) {
         setCallBackFunction()
 
         // 유효성 검사
-
+        checkString()
 
 
     }
-    private fun setCallBackFunction(){
+
+    private fun setCallBackFunction() {
         // Ok 버튼
-        binding.btnAddContactOk.setOnClickListener {
-            // 데이터 전달
-            // 종료
-            dismiss()
+        binding.btnAddContactOk.run {
+            // 유효성 검사 전 확인 버튼 비활성화
+            isEnabled = false
+            setOnClickListener {
+
+
+                // 데이터 전달
+
+
+                // 종료
+                dismiss()
+            }
         }
 
         // 뒤로가기 버튼
@@ -37,6 +55,75 @@ class AddContactDialog(context : Context) : Dialog(context) {
             //종료
             dismiss()
         }
+
+
+    }
+
+    private fun checkString() {
+        // 유효성 검사
+        val etName = binding.etAddContactName
+        val etPhoneNum = binding.etAddContactPhoneNumber
+        val etEmail = binding.etAddContactEmail
+
+        val btnOk = binding.btnAddContactOk
+
+
+        // 이름 유효성 검사
+        etName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //텍스트 바뀌기 전
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //텍스트 바뀌는 도중
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //텍스트 바뀐후 수행
+                isCheckedName = CheckString().checkName(s.toString())
+                isChecked = isCheckedName && isCheckedEmail && isCheckedPhoneNum
+                btnOk.isEnabled = isChecked
+            }
+
+        })
+
+        //전화 번호 유효성 검사
+        etPhoneNum.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //텍스트 바뀌기 전
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //텍스트 바뀌는 도중
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //텍스트 바뀐후 수행
+                isCheckedPhoneNum = CheckString().checkPhoneNumber(s.toString())
+                isChecked = isCheckedName && isCheckedEmail && isCheckedPhoneNum
+                btnOk.isEnabled = isChecked
+            }
+
+        })
+
+        //이메일 유효성 검사
+        etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //텍스트 바뀌기 전
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //텍스트 바뀌는 도중
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //텍스트 바뀐후 수행
+                isCheckedEmail = CheckString().checkEmail(s.toString())
+                isChecked = isCheckedName && isCheckedEmail && isCheckedPhoneNum
+                btnOk.isEnabled = isChecked
+            }
+
+        })
     }
 
 
