10c4ef1675178aceb7310c1b2c0429413737f90f
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-01-18 04:31:19 +0900

    Feat : gradle activity, glide 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
@@ -58,4 +59,8 @@ dependencies {
 
     // motion
     implementation ("androidx.constraintlayout:constraintlayout:2.0.0-beta1")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 }
