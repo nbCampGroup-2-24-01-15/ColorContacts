61f4ecae868e49cdfeb6bf9c85adbd0e388e7dd7
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-01-18 16:52:56 +0900

    Feat : implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,6 @@ dependencies {
     // uri image
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-}
 
     //Glide
     implementation("com.github.bumptech.glide:glide:4.12.0")
